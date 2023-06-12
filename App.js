42363a289c5bf056579b18a0f5393b2e961d6b47
Palona <lainedaridan.as@gmail.com>
2023-06-12 10:13:16 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import user from './reducers/user';
+
+const store = configureStore({
+  reducer: { user },
+});
+
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import PlacesScreen from './screens/PlacesScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = '';
+
+          if (route.name === 'Map') {
+            iconName = 'location-arrow';
+          } else if (route.name === 'Places') {
+            iconName = 'map-pin';
+          }
+
+          return <FontAwesome name={iconName} color={color} size={size} />;
+        },
+        tabBarActiveTintColor: '#B733D0',
+        tabBarInactiveTintColor: '#335561',
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Places" component={PlacesScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
